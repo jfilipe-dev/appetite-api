38cca8b9933ecb03e3949a700212d82ec5307784
jfilipe-dev <jfilipe1511@gmail.com>
2020-07-31 19:00:13 -0300

    Initial commit

---
+++ src/shared/infra/http/server.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const server = express();
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+server.listen(3333, () => {
+  console.log('👍 Server started on port 3333!');
+});
